2954297cfb8395211dd5100f0442503617113b15
Marijan Petričević <marijan.petricevic94@gmail.com>
2025-04-26 11:22:20 -0500

    move to soupault

--- flake.nix
+++ flake.nix
@@ -2,54 +2,65 @@
   description = "simplistic's website";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/8a2f738d9d1f1d986b5a4cd2fd2061a7127237d7";
     flake-parts.url = "github:hercules-ci/flake-parts";
     treefmt-nix.url = "github:numtide/treefmt-nix";
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
-    npmlock2nix.url = "github:nix-community/npmlock2nix";
+    npmlock2nix.url = "github:nix-community/npmlock2nix/9197bbf397d76059a76310523d45df10d2e4ca81";
     npmlock2nix.flake = false;
-    website-builder.url = "git+https://git.sr.ht/~marijan/website-builder";
-    #website-builder.url = "path:/home/marijan/workspace/website-builder";
   };
 
-  outputs = inputs@{ self, flake-parts, treefmt-nix, ... }:
+  outputs =
+    inputs@{ flake-parts, treefmt-nix, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" ];
       imports = [
         treefmt-nix.flakeModule
       ];
-      perSystem = { config, self', inputs', pkgs, system, lib, ... }:
+      flake = {
+        overlays.default = import ./overlay.nix;
+      };
+      perSystem =
         {
+          system,
+          self',
+          pkgs,
+          lib,
+          ...
+        }:
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              (final: prev: {
+                nodejs-16_x = final.nodejs;
+                npmlock2nix = pkgs.callPackage inputs.npmlock2nix { };
+              })
+              (import ./overlay.nix)
+            ];
+            config = { };
+          };
           treefmt = {
             projectRootFile = ".git/config";
-            programs.nixpkgs-fmt.enable = true;
+            programs.nixfmt-rfc-style.enable = true;
           };
 
           apps.gh-deploy = {
             type = "app";
-            program = "${pkgs.writeShellApplication {
-              name = "gh-deploy";
-              runtimeInputs = [ ];
-              text = ''
-                cp -r ${self'.packages.dist}/* docs
-              '';
-            }}/bin/gh-deploy";
+            program = "${lib.getExe (
+              pkgs.writeShellApplication {
+                name = "gh-deploy";
+                runtimeInputs = [ pkgs.coreutils ];
+                text = ''
+                  cp --no-preserve=mode -r ${self'.packages.dist}/* docs
+                '';
+              }
+            )}";
           };
 
           packages = {
-            dist =
-              let
-                npmlock2nix = import inputs.npmlock2nix { inherit pkgs; };
-              in
-              pkgs.runCommand "dist" { LANG = "en_US.UTF-8"; nativeBuildInputs = [ pkgs.nodePackages.tailwindcss ]; } ''
-                mkdir -p $out
-                cp -r ${./src}/* .
-                ${lib.getExe inputs'.website-builder.packages.default} build
-                cp -r docs/* $out/
-                export NODE_PATH=${npmlock2nix.v2.node_modules { src = ./.; nodejs = pkgs.nodejs; }}/node_modules
-                cd $out
-                tailwindcss -c ${./tailwind.config.js} -i css/style.css -o css/style.css
-              '';
+            inherit (pkgs) dist;
+            default = self'.packages.dist;
           };
         };
     };
